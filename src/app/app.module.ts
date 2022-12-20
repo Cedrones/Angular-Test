bbaafebf238b02d6b529afa13d8276069c38e9eb
Cedrones <sebastian-niechcial@wp.pl>
2022-12-20 17:38:11 +0100

    Added tasks and some minor tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TasksItemComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Dentist',
+    day: 'April 6th at 3:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Der Arzt',
+    day: 'April 10th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Schule',
+    day: 'April 17th at 2:30pm',
+    reminder: false,
+  },
+];
